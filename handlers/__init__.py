b8384a10657162e2604941144beef771f7f68fbc
ghadeerreda0-lab <ghadeer.reda.0@gmail.com>
2026-01-19 00:30:07 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,11 @@
- 
+ from .start import router as start_router
+from .charge.main import router as charge_router
+from .charge.syriatel import router as syriatel_router
+from .withdraw.main import router as withdraw_router
+
+__all__ = [
+    'start_router',
+    'charge_router', 
+    'syriatel_router',
+    'withdraw_router'
+]
